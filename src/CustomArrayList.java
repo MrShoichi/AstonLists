bd7767b8227086c4f7820054c560a34c33861385
MrShoichi <avvlad2002@mail.ru>
2025-01-28 09:45:00 +0300

    Add lists implemantations

---
+++ src/CustomArrayList.java
@@ -0,0 +1,243 @@
+import java.util.Comparator;
+
+/**
+ * CustomArrayList - реализация списка с динамическим массивом.
+ * <p>
+ * Этот класс предоставляет основные операции работы со списком.
+ *
+ * @param <T> тип элементов списка
+ */
+public class CustomArrayList<T> implements ICustomList<T> {
+
+    /**
+     * Массив для хранения элементов
+     */
+    private T[] array;
+    /**
+     * Количество элементов в списке
+     */
+    private int size;
+    /**
+     * Стандартный размер списка
+     */
+    private static final int DEFAULT_CAPACITY = 16;
+    /**
+     * Текущий размер списка
+     */
+    private int capacity;
+
+    /**
+     * Конструктор для создания списка с заданным размером.
+     *
+     * @param capacity начальный размер списка.
+     */
+    public CustomArrayList(int capacity) {
+        this.capacity = capacity;
+        this.array = (T[]) new Object[capacity];
+        this.size = 0;
+    }
+
+    /**
+     * Базовый конструктор с базовым размером - 16.
+     */
+    public CustomArrayList() {
+        this(DEFAULT_CAPACITY);
+    }
+
+
+    /**
+     * Метод добавления элемента в конец списка.
+     * При переполнении списка, размер списка увеличивается.
+     * Скорость O(1)/ При увеличении размера O(n).
+     *
+     * @param o добавляемый элемент.
+     */
+    @Override
+    public void add(T o) {
+        if (size == capacity) {
+            resize(capacity * 2);
+        }
+        array[size++] = o;
+    }
+
+    /**
+     * Метод вставки элемента на указанный существующую позицию в списке.
+     * Скорость O(n).
+     *
+     * @param index индекс в списке.
+     * @param o добавляемый элемнт.
+     * @throws IndexOutOfBoundsException если индекс зашел за границы списка. (index < 0 || index >= size)
+     */
+    @Override
+    public void add(int index, T o) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (size == capacity) {
+            resize(capacity * 2);
+        }
+
+
+        for (int i = size++; i >= index + 1; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = o;
+    }
+
+    /**
+     * Метод изменения размера списка
+     *
+     * @param newCapacity новый размер списка
+     */
+    private void resize(int newCapacity) {
+        var temp = array;
+        capacity = newCapacity;
+        array = (T[]) new Object[newCapacity];
+        System.arraycopy(temp, 0, array, 0, temp.length);
+    }
+
+    /**
+     * Метод получения элемента списка на указанной позиции.
+     * Скорость O(1).
+     *
+     * @param index индекс в списке.
+     * @return элемент списка.
+     * @throws IndexOutOfBoundsException если индекс зашел за границы списка. (index < 0 || index >= size)
+     */
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return array[index];
+    }
+
+    /**
+     * Метод удаления элемента списка на указанной позиции.
+     * Средняя скорость O(n).
+     *
+     * @param index индекс удаляемого элемента.
+     * @return удаленный элемен.
+     * @throws IndexOutOfBoundsException если индекс зашел за границы списка. (index < 0 || index >= size)
+     */
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        var temp = array[index];
+        size--;
+        System.arraycopy(array, index + 1, array, index, size - index);
+        return temp;
+    }
+
+
+    /**
+     * Метод удаления определенного элемента списка по значению.
+     * Средняя скорость O(n).
+     *
+     * @param o удаляемый элемент списка.
+     * @return True если элемент был удален, иначе False
+     */
+    @Override
+    public boolean remove(T o) {
+        int index = find(o);
+        if (index == -1) {
+            return false;
+        }
+        size--;
+        System.arraycopy(array, index + 1, array, index, size - index);
+        return true;
+    }
+
+
+    private int find(T o) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    /**
+     * Метод очистки списка. Размер списка вернется в базовое состояние - 16.
+     */
+    @Override
+    public void clear() {
+        capacity = DEFAULT_CAPACITY;
+        size = 0;
+        array = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    /**
+     * Метод сортировки списка. Метод принимает компаратор и
+     * использует его для сравнения элементов списка и их сортировки.
+     * Средняя скорость O(n^2).
+     *
+     * @param c Компоратор для сравнения элементов списка
+     */
+    @Override
+    public void sort(Comparator<? super T> c) {
+        if (size == 0) {
+            return;
+        }
+        for (int i = 1; i < size; i++) {
+            for (int j = 0; j < size - i; j++) {
+                if (c.compare(array[j], array[j + 1]) >= 0) {
+                    var temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    /**
+     * Метод получения размера списка.
+     *
+     * @return Размер списка
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Метод поиска элемента в списке.
+     *
+     * @param o элемент, чей поиск будет производиться.
+     * @return True если такой элемент есть в списке, иначе False
+     */
+    @Override
+    public boolean contains(T o) {
+        return find(o) != -1;
+    }
+
+    /**
+     * Метод проверки пустой ли список.
+     *
+     * @return True если список пустой, иначе False
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        if (size == 0) {
+            return "[]";
+        }
+        var arrayString = new StringBuilder("[");
+        for (int i = 0; i < size - 1; i++) {
+            arrayString.append(array[i]).append(", ");
+        }
+        arrayString.append(array[size - 1]);
+        arrayString.append("]");
+        return arrayString.toString();
+    }
+
+
+}
---
+++ src/CustomLinkedList.java
@@ -0,0 +1,408 @@
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+/**
+ * CustomLinkedList - реализация двусвязного списка. Класс предоставляет основные методы работы со списком.
+ * @param <T> тип элементов списка
+ */
+public class CustomLinkedList<T> implements ICustomList<T> {
+
+    /**
+     * Начало списка.
+     */
+    private Node<T> head;
+    /**
+     * Конец списка
+     */
+    private Node<T> tail;
+    /**
+     * Количество элементов списка
+     */
+    private int size;
+
+
+    /**
+     * Метод добавления элемента в список.
+     * Скорость O(1).
+     * @param o добавляемый элемент.
+     */
+    @Override
+    public void add(T o) {
+        addLast(o);
+    }
+
+    /**
+     * Метод вставки элемента на определенную позицию списка.
+     * Средняя скорость O(n).
+     *
+     * @param index индекс в списке.
+     * @param o добавляемый элемент.
+     * @throws IndexOutOfBoundsException Если индекс зашел за границы списка. (index < 0 || index >= size)
+     */
+    @Override
+    public void add(int index, T o) {
+        if (size < index || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            addFirst(o);
+            return;
+        }
+
+        var existsNode = getNode(index);
+        existsNode.last.next = new Node<>(o, existsNode, existsNode.last);
+        existsNode.last = existsNode.last.next;
+        size++;
+
+    }
+
+    /**
+     * Метод добавления элемента в начало списка.
+     * Скорость O(1)
+     *
+     * @param o добавляемый элемент.
+     */
+    public void addFirst(T o) {
+        var node = new Node<>(o);
+        if (head != null) {
+            head.last = node;
+        }
+        if (tail == null) {
+            tail = node;
+        }
+        node.next = head;
+        head = node;
+
+        size++;
+    }
+
+    /**
+     * Метод добавления элемента в конец списка.
+     * Скорость O(1)
+     *
+     * @param o добавляемый элемент.
+     */
+    public void addLast(T o) {
+        var node = new Node<>(o, tail);
+
+        if (tail != null) {
+            tail.next = node;
+        }
+        if (head == null) {
+            head = node;
+        }
+        tail = node;
+
+        size++;
+    }
+
+    /**
+     * Метод получения элемента списка на позиции.
+     * Средняя скорость O(n).
+     *
+     * @param index индекс элемента.
+     * @return элемент списка.
+     * @throws IndexOutOfBoundsException Если индекс зашел за границы списка. (index < 0 || index >= size)
+     */
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return getNode(index).data;
+    }
+
+    /**
+     * Метод удаления элемента списка на позиции.
+     * Средняя скорость O(n).
+     *
+     * @param index индекс элемента.
+     * @return удаленный элемент списка.
+     * @throws IndexOutOfBoundsException Если индекс зашел за границы списка. (index < 0 || index >= size)
+     */
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            return removeFirst();
+        }
+        if (index == size - 1) {
+            return removeLast();
+        }
+
+        var node = getNode(index);
+        node.next.last = node.last;
+        node.last.next = node.next;
+        size--;
+        return node.data;
+    }
+
+    /**
+     * Метод удаления последнего элемента списка.
+     * Скорость O(1).
+     *
+     * @return удаленный элемент списка.
+     * @throws NoSuchElementException Если список пуст.
+     */
+    public T removeLast() {
+        if (tail == null) {
+            throw new NoSuchElementException();
+        }
+        var temp = tail;
+        if (size != 1) {
+            tail.last.next = null;
+            tail = tail.last;
+        } else {
+            clear();
+        }
+        size--;
+        return temp.data;
+    }
+    /**
+     * Метод удаления первого элемента списка.
+     * Скорость O(1).
+     *
+     * @return удаленный элемент списка.
+     * @throws NoSuchElementException Если список пуст.
+     */
+    public T removeFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        if(size == 1) {
+            var tempHead = head;
+            clear();
+            return tempHead.data;
+        }
+        head.next.last = null;
+        head = head.next;
+
+        size--;
+
+        return null;
+    }
+
+    /**
+     * Метод удаления определеноого элемента из списка.
+     * Средня скорость O(n).
+     *
+     * @param o удаляемый элемен списка.
+     * @return True если элемент был удален, иначе False.
+     */
+    @Override
+    public boolean remove(T o) {
+        var node = getNode(o);
+        if (node == null) {
+            return false;
+        }
+        if (head == node) {
+            removeFirst();
+        } else if (tail == node) {
+            removeLast();
+        } else {
+            node.next.last = node.last;
+            node.last.next = node.next;
+            size--;
+        }
+        return true;
+    }
+
+    /**
+     * Метод поиска узла по индексу.
+     *
+     * @param index "индекс" узла.
+     * @return найденный узел.
+     */
+    private Node<T> getNode(int index) {
+        var node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+
+    /**
+     * Метод поиска узла по данным.
+     *
+     * @param o данные узла.
+     * @return найденный узел.
+     */
+    private Node<T> getNode(T o) {
+        var node = head;
+        while (node != null) {
+            if(node.data.equals(o))
+                return node;
+            node = node.next;
+        }
+        return node;
+    }
+
+
+    /**
+     * Метод очистки списка.
+     */
+    @Override
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    /**
+     * Метод сортировки списка. Метод принимает компаратор и
+     * использует его для сравнения элементов списка и их сортировки.
+     * Средняя скорость O(n^2).
+     *
+     * @param c Компоратор для сравнения элементов списка
+     */
+    @Override
+    public void sort(Comparator<? super T> c) {
+        if (size <= 2) {
+            return;
+        }
+        for (int i = 1; i < size; i++) {
+            var firstNode = head;
+            for (int j = 0; j < size - i; j++) {
+                var nextNode = firstNode.next;
+                if (c.compare(firstNode.data, nextNode.data) > 0) {
+                    if(firstNode == head) {
+                        head = nextNode;
+                    }
+                    if(nextNode == tail) {
+                        tail = firstNode;
+                    }
+                    swap(firstNode, nextNode);
+                }
+                else {
+                    firstNode = nextNode;
+                }
+            }
+        }
+    }
+
+    /**
+     * Метод замены местами двух узлов в двусвязном списке.
+     * Метод меняет ссылкы двух узлов так, чтобы поменять их местами.
+     *
+     * @param firstNode Первый узел
+     * @param secondNode Второй узел
+     */
+    private static <T> void swap(Node<T> firstNode, Node<T> secondNode) {
+        if(firstNode.last != null) {
+            firstNode.last.next = secondNode;
+        }
+        if(secondNode.next != null) {
+            secondNode.next.last = firstNode;
+        }
+        firstNode.next = secondNode.next;
+        secondNode.last = firstNode.last;
+        firstNode.last = secondNode;
+        secondNode.next = firstNode;
+    }
+
+    /**
+     * Метод получения размера списка.
+     *
+     * @return Размер списка
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Метод поиска элемента в списке.
+     *
+     * @param o элемент, чей поиск будет производиться.
+     * @return True если такой элемент есть в списке, иначе False
+     */
+    @Override
+    public boolean contains(T o) {
+        return getNode(o) != null;
+    }
+
+    /**
+     * Метод проверки пустой ли список.
+     *
+     * @return True если список пустой, иначе False
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        if (head == null) {
+            return "[]";
+        }
+        var node = head;
+        var arrayString = new StringBuilder("[");
+        while (node.next != null) {
+            arrayString.append(node.data).append(", ");
+            node = node.next;
+        }
+        arrayString.append(node.data);
+        arrayString.append("]");
+        return arrayString.toString();
+    }
+
+    /**
+     * Класс реализация узла списка
+     * @param <T> тип хранимых данных узлом
+     */
+    private static class Node<T> {
+        /**
+         * Данные узла
+         */
+        T data;
+        /**
+         * Ссылка на следующий узел
+         */
+        Node<T> next;
+        /**
+         * Ссылка на следующий предыдущий узел
+         */
+        Node<T> last;
+
+        /**
+         * Конструктор для создания узла c данными и ссылками.
+         *
+         * @param data данные узла.
+         * @param next ссылка на следующий узел.
+         * @param last ссылка на предыдущий узел.
+         */
+        public Node(T data, Node<T> next, Node<T> last) {
+            this.data = data;
+            this.next = next;
+            this.last = last;
+        }
+
+        /**
+         * Конструктор для создания узла c данными.
+         * Ссылки по-умолчанию равны null.
+         *
+         * @param data данные узла.
+         */
+        public Node(T data) {
+            this(data, null, null);
+        }
+
+        /**
+         * Конструктор для создания узла c данными и ссылкой на прошлый узел.
+         * Ссылка на следующий узел по-умолчанию равны null.
+         *
+         * @param data данные узла.
+         * @param last ссылка на предыдущий узел.
+         */
+        public Node(T data, Node<T> last) {
+            this(data, null, last);
+        }
+
+
+    }
+
+}
